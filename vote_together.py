4ec7cc48852ae35d13406e165b060aa463c70cb3
Forest Gregg <fgregg@uchicago.edu>
2018-09-25 18:53:00 -0500

    resort

--- vote_together.py
+++ vote_together.py
@@ -1,29 +1,36 @@
 import csv
 import sys
+import igraph
 
 import numpy
 
+import matplotlib.pyplot as plt
+
 
 reader = csv.reader(sys.stdin)
+
 alders = numpy.array(next(reader)[3:])
 
 votes = numpy.array(list(reader))
 votes = votes[:, 3:]
 
-votes = (votes == 'no').astype(int)
+no_votes = -((votes == 'no').astype(int))
+yes_votes = (votes == 'yes').astype(int)
 
-vote_together = numpy.dot(votes.T, votes)
+votes = no_votes + yes_votes
 
-u, s, vh = numpy.linalg.svd(vote_together)
+u, s, vh = numpy.linalg.svd(votes.T)
 
 dim_1 = numpy.argsort(u[:, 0])
-dim_2 = numpy.argsort(u[:, 1])
+dim_2 = numpy.argsort(-u[:, 1])
 
-vote_together = vote_together[dim_1, :][:, dim_1]
+oppose_together = numpy.dot(no_votes.T, no_votes)
+
+oppose_together = oppose_together[dim_2, :][:, dim_2]
 
 writer = csv.writer(sys.stdout)
 
-writer.writerow([''] + list(alders[dim_1]))
-for alder, row in zip(alders[dim_1], vote_together):
-    writer.writerow([alder] + list(row.astype(str)))
+writer.writerow([''] + list(alders[dim_2]))
+for alder, row in zip(alders[dim_2], oppose_together):
+     writer.writerow([alder] + list(row.astype(str)))
 
